6beb363bb99e69fe15b2c54a02cae6667cac2071
e1810 <v.iceele1810@gmail.com>
2020-09-10 00:59:02 +0900

    バーサライタの軽い再現を実装

---
+++ README.txt
@@ -0,0 +1,7 @@
+ビルド&実行
+cmake .
+make
+./RotateLED
+
+参考資料
+https://www.trifields.jp/how-to-install-opencv-on-ubuntu-2742
---
+++ build/CMakeFiles/RotateLED.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RotateLED.dir/test.cpp.o"
+  "RotateLED"
+  "RotateLED.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RotateLED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
